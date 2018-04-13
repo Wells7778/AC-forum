75008003f36c033046401de4251c5447aabd946a
Wells <chawkeye@gmail.com>
2018-04-13 20:32:23 +0800

    撰寫產生 token 的方法

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,8 @@ class User < ApplicationRecord
 
   mount_uploader :avatar, AvatarUploader
 
+  before_create :generate_authentication_token
+
   ROLE = {
     normal: "一般用戶",
     admin: "管理者"
@@ -49,5 +51,9 @@ class User < ApplicationRecord
   def all_friends
     friends = self.friends + self.inverse_friends
     return friends.uniq
+  end
+
+  def generate_authentication_token
+     self.authentication_token = Devise.friendly_token
   end
 end

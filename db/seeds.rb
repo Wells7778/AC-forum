151b16fa14798f9843c48ab9d497a0127464b4cf
Wells <chawkeye@gmail.com>
2018-04-13 14:44:16 +0800

    增加管理者帳號

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: "admin@example.com", password: "12345678", role: "ADMIN", name: "Admin")
+puts "create admin account"

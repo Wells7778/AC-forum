05eb261f176bdece00afa2a28d4f50703609d92c
Wells <chawkeye@gmail.com>
2018-04-13 14:12:30 +0800

    設定devise parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role, :avatar, :description])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :role, :avatar, :description])
+  end
 end

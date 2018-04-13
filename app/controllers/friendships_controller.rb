6c5b82052162b0e64d8a573c427de86a1f41ff98
Wells <chawkeye@gmail.com>
2018-04-13 16:38:32 +0800

    用AJAX建立friendships#create/friendships#accept/friendships#ignore

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,28 @@
-class FriendshipsController < ApplicationController
+class FriendshipsController < BaseController
+  def create
+    @friendship = current_user.not_yet_accepted_by_friendships.build(friend_id: params[:friend_id])
+    @friendship.save
+    @user = User.find(params[:friend_id])
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def accept
+    @friendship = current_user.not_yet_responded_to_friendships.where(user_id: params[:id]).first
+    @friendship.update(status: true)
+    @user = User.find(params[:id])
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def ignore
+    @friendship = current_user.not_yet_responded_to_friendships.where(user_id: params[:id]).first
+    @friendship.destroy
+    @user = User.find(params[:id])
+    respond_to do |format|
+      format.js
+    end
+  end
 end

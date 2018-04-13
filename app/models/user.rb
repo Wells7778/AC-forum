7594ffc78600e03364a3a72e1a0fe710b521f180
Wells <chawkeye@gmail.com>
2018-04-13 16:04:47 +0800

    建立好友關聯

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,9 +15,35 @@ class User < ApplicationRecord
   has_many :collections, dependent: :destroy
   has_many :collect_posts, through: :collections, source: :post
 
+  has_many :friendships, -> {where status: true}, dependent: :destroy
+  has_many :friends, through: :friendships
+  has_many :inverse_friendships, -> {where status: true}, class_name: "Friendship", foreign_key: "friend_id"
+  has_many :inverse_friends, through: :inverse_friendships, source: :user
+  has_many :not_yet_accepted_by_friendships, -> {where status: false}, class_name: "Friendship", dependent: :destroy
+  has_many :not_yet_accepted_by_friends, through: :not_yet_accepted_by_friendships, source: :friend
+  has_many :not_yet_responded_to_friendships, -> {where status: false}, class_name: "Friendship", foreign_key: "friend_id", dependent: :destroy
+  has_many :not_yet_responded_to_friends, through: :not_yet_responded_to_friendships, source: :user
+
   mount_uploader :avatar, AvatarUploader
 
   def admin?
     self.role == "admin"
+  end
+
+  def friend?(user)
+    self.friends.include?(user) || self.inverse_friends.include?(user)
+  end
+
+  def not_yet_accepted_by?(user)
+    self.not_yet_accepted_by_friends.include?(user)
+  end
+
+  def not_yet_responded_to?(user)
+    self.not_yet_responded_to_friends.include?(user)
+  end
+
+  def all_friends
+    friends = self.friends + self.inverse_friends
+    return friends.uniq
   end
 end

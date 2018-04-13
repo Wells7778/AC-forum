55684df21d1b373b6e0212f0178e8e209a4d4e56
Wells <chawkeye@gmail.com>
2018-04-13 16:47:51 +0800

    完成feeds頁面

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,2 +1,9 @@
-class FeedsController < ApplicationController
+class FeedsController < BaseController
+  def index
+    @user_count = User.all.count
+    @post_count = Post.all.count
+    @comment_count = Comment.all.count
+    @users = User.select(:id, :name, :comments_count).order(comments_count: :desc).limit(10)
+    @posts = Post.select(:id, :title, :comments_count, :updated_at).order(comments_count: :desc).limit(10)
+  end
 end

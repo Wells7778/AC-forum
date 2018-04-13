11494ed7ca83c586cca31d94c255748fc578a2f0
Wells <chawkeye@gmail.com>
2018-04-13 15:23:22 +0800

    post掛上uploader可上傳檔案

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,6 +10,8 @@ class Post < ApplicationRecord
   has_many :vieweds, dependent: :destroy
   has_many :viewed_users, through: :vieweds, source: :user
 
+  mount_uploader :image, ImageUploader
+
   scope :open_public, -> { where(public: true) }
 
   def self.readable_posts(user)

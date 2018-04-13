c1abb5790f41c31b63a12e1304b63cd9d8254593
Wells <chawkeye@gmail.com>
2018-04-13 15:18:00 +0800

    完成post#edit post#update post#destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: :index
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :check_author, only: [:edit, :update, :destroy]
 
   def index
     @categories = Category.all
@@ -45,6 +46,26 @@ class PostsController < ApplicationController
     @comment = Comment.new
   end
 
+  def update
+    if params[:commit] == "Save Draft"
+      @post.public = false
+    else
+      @post.public = true
+    end
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      flash.now[:alert] = @post.errors.full_messages.to_sentence
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:notice] = "文章已刪除"
+    redirect_to posts_path
+  end
+
   private
 
   def post_params
@@ -53,5 +74,12 @@ class PostsController < ApplicationController
 
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def check_author
+    unless @post.user == current_user || current_user.admin?
+      flash[:alert] = "這不是您的文章喔！"
+      redirect_to post_path(@post)
+    end
   end
 end

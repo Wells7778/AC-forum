4c30c98470ed076a7e694d5003666dce4c2e8345
Wells <chawkeye@gmail.com>
2018-04-13 14:33:46 +0800

    完成post index頁面／設定好boostrap4

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,12 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
+  def index
+    if current_user
+        @ransack = Post.readable_posts(current_user).open_public.ransack(params[:q])
+    else
+        @ransack = Post.open_public.where(authority: "all").ransack(params[:q])
+    end
+      @posts = @ransack.result(distinct: true).page(params[:page]).per(20)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
 
   scope :open_public, -> { where(public: true) }
+
+  def self.readable_posts(user)
+    Post.where(authority: "Friend", user: user.all_friends).or( where(authority: "All")).or(where(authority: "Myself", user: user))
+  end
 end

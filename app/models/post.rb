144f595227352f2d5edb0da7efdad51ca53fce62
Wells <chawkeye@gmail.com>
2018-04-13 17:17:32 +0800

    修正首頁文章權限

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,7 +18,7 @@ class Post < ApplicationRecord
   scope :open_public, -> { where(public: true) }
 
   def self.readable_posts(user)
-    Post.where(authority: "friend", user: user.all_friends).or( where(authority: "all")).or(where(authority: "myself", user: user))
+    Post.where(authority: "friend", user: user.all_friends).or( where(authority: "all")).or(where(authority: "myself", user: user)).or(where( user: user))
   end
 
   def viewed_by?(user)

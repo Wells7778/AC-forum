1d9a75a6036864b43cb3f419a75e79ff503e899b
Wells <chawkeye@gmail.com>
2018-04-13 15:00:33 +0800

    建立Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180413063554) do
+ActiveRecord::Schema.define(version: 20180413065935) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20180413063554) do
   create_table "categories_posts", force: :cascade do |t|
     t.integer "post_id"
     t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

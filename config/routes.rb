555a7e721e4d0575bfeacc80f984646407bbe38b
Wells <chawkeye@gmail.com>
2018-04-13 15:48:44 +0800

    設定collect/uncollect的routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   end
   resources :posts do
     resources :comments, only: [:create, :destroy]
+    member do
+      post :collect
+      post :uncollect
+    end
   end
 
   root "posts#index"

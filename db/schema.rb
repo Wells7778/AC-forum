e04a34f35d767ce2512258495ec6aef614516230
Wells <chawkeye@gmail.com>
2018-04-13 14:35:21 +0800

    建立Category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180413062219) do
+ActiveRecord::Schema.define(version: 20180413063429) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false

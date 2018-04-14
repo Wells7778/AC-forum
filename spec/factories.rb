bd81a8fb435d6315466bf37e631533cd37eeb41b
Wells <chawkeye@gmail.com>
2018-04-14 17:07:16 +0800

    設定rspec測試環境

---
+++ spec/factories.rb
@@ -0,0 +1,51 @@
+FactoryBot.define do
+  factory :post do
+    title FFaker::Book.unique.author
+    content FFaker::Book.description
+    user
+    public true
+
+    factory :draft do
+      public false
+    end
+
+    factory :post_for_all do
+      authority "all"
+    end
+
+    factory :post_for_friend do
+      authority "friend"
+    end
+
+    factory :post_for_self do
+      authority "myself"
+    end
+  end
+
+  factory :comment do
+    content
+    user
+    post
+  end
+
+  factory :collect do
+    user
+    post
+  end
+
+  factory :view do
+    user
+    post
+  end
+
+  factory :user do
+    name FFaker::Name.unique.last_name
+    email FFaker::Internet.email
+    password "12345678"
+    description FFaker::Lorem.paragraphs
+
+    factory :admin do
+      role "admin"
+    end
+  end
+end

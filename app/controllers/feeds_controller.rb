17fa811d4890536d9e139715419426ef8fe3541c
Wells <chawkeye@gmail.com>
2018-04-15 22:12:43 +0800

    修正feeds頁面的post數量，拿掉草稿的數量

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,7 +1,7 @@
 class FeedsController < BaseController
   def index
     @user_count = User.all.count
-    @post_count = Post.all.count
+    @post_count = Post.open_public.count
     @comment_count = Comment.all.count
     @users = User.select(:id, :name, :comments_count).order(comments_count: :desc).limit(10)
     @posts = Post.select(:id, :title, :comments_count, :updated_at).order(comments_count: :desc).limit(10)

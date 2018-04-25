a9b0bf36c3150deca8c3463e164a9956f31efb10
Wells <chawkeye@gmail.com>
2018-04-25 11:38:37 +0800

    把更新post裏last_replied_at部分程式碼刪掉，手動更新

--- db/migrate/20180425032231_add_last_reply_at_to_posts.rb
+++ db/migrate/20180425032231_add_last_reply_at_to_posts.rb
@@ -2,10 +2,4 @@ class AddLastReplyAtToPosts < ActiveRecord::Migration[5.1]
   def change
     add_column :posts, :last_replied_at, :datetime
   end
-  Post.all.each do |post|
-    if post.comments.present?
-      post.last_replied_at = post.comments.last.created_at
-      post.save
-    end
-  end
 end

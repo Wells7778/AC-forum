468af4bc138ef19be9183b4ad9da2165b2ad7172
Wells <chawkeye@gmail.com>
2018-04-25 11:33:23 +0800

    更新依照最後回覆時間排序

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :user, counter_cache: true
   belongs_to :post, counter_cache: true
-
+  after_save :update_post_last_replied_at
   validates_presence_of :content
+
+  def update_post_last_replied_at
+    self.post.update(last_replied_at: self.updated_at)
+  end
 end

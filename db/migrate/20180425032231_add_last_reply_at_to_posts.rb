16e154d78e5e63139d5e87a446f051dd64ad225b
Wells <chawkeye@gmail.com>
2018-04-25 11:35:20 +0800

    修拼字錯誤

--- db/migrate/20180425032231_add_last_reply_at_to_posts.rb
+++ db/migrate/20180425032231_add_last_reply_at_to_posts.rb
@@ -3,7 +3,7 @@ class AddLastReplyAtToPosts < ActiveRecord::Migration[5.1]
     add_column :posts, :last_replied_at, :datetime
   end
   Post.all.each do |post|
-    if post.comments.presents?
+    if post.comments.present?
       post.last_replied_at = post.comments.last.created_at
       post.save
     end

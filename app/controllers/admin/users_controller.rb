d0ef7942f1031963851dc6976b965cb533be5cdf
Wells <chawkeye@gmail.com>
2018-05-02 08:44:29 +0800

    設定無法修改admin@example.com的權限

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,9 +5,13 @@ class Admin::UsersController < Admin::BaseController
 
   def update
     @user = User.find(params[:id])
-    @user.update(role: params[:role])
-    flash[:notice] = "#{@user.name}權限更新為#{params[:role]}"
-    redirect_to admin_users_path
+    if @user.email == "admin@example.com"
+      flash[:alert] = "網站管理者權限無法修改"
+      redirect_to admin_users_path
+    else
+      @user.update(role: params[:role])
+      flash[:notice] = "#{@user.name}權限更新為#{params[:role]}"
+      redirect_to admin_users_path
+    end
   end
-
 end
